32c1a662609f8b4a1d75031f3d5e28ee76c1bd28
so1omon <solmin3665@gmail.com>
2022-08-05 19:27:12 +0900

    sequelize DB접근 명령어 변경

--- models/index.js
+++ models/index.js
@@ -8,8 +8,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize= new Sequelize(config.database, config.username, config.password, config);
-
+// const sequelize= new Sequelize(config.database, config.username, config.password, config);
+const sequelize= new Sequelize(process.env.URI);
 fs
   .readdirSync(__dirname)
   .filter(file => {
