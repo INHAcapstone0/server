21c82d04bcdd6d9ebff6421a2a587947928f3956
so1omon <solmin3665@gmail.com>
2022-08-05 20:55:35 +0900

    config.js 변경

--- config/config.js
+++ config/config.js
@@ -7,18 +7,12 @@ const development = {
   database: env.MYSQL_DATABASE,
   host: env.MYSQL_HOST,
   dialect: env.MYSQL_DIALECT || "mysql",
-  port: env.MYSQL_PORT,
-  uri: env.URI
+  port: env.MYSQL_PORT
 };
 
 const production = {
-  username: env.MYSQL_USERNAMET_PRODUCTION,
-  password: env.MYSQL_PASSWORDT_PRODUCTION,
-  database: env.MYSQL_DATABASET_PRODUCTION,
-  host: env.MYSQL_HOSTT_PRODUCTION,
-  dialect: env.MYSQL_DIALECT||"mysql",
-  port: env.MYSQL_PORT || 3306,
-  uri: env.URI
+  use_env_variable: env.URI,
+  "dialect": "mysql"
 };
 
 const test = {
--- models/index.js
+++ models/index.js
@@ -10,11 +10,12 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 console.log(env);
+
 let sequelize;
-if (env=="development"){
-  sequelize= new Sequelize(config.database, config.username, config.password, config);
-}else if (env=="production"){
-  sequelize= new Sequelize(config.uri);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 // const sequelize= new Sequelize(config.database, config.username, config.password, config);
 
