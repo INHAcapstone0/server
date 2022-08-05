e9ac480fe264d94032485e8c7f9da6c9e9e7a137
so1omon <solmin3665@gmail.com>
2022-08-05 20:46:09 +0900

    config.js 및 .env 변경

--- config/config.js
+++ config/config.js
@@ -12,12 +12,12 @@ const development = {
 };
 
 const production = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
+  username: env.MYSQL_USERNAMET_PRODUCTION,
+  password: env.MYSQL_PASSWORDT_PRODUCTION,
+  database: env.MYSQL_DATABASET_PRODUCTION,
+  host: env.MYSQL_HOSTT_PRODUCTION,
   dialect: env.MYSQL_DIALECT||"mysql",
-  port: env.MYSQL_PORT,
+  port: env.MYSQL_PORT || 3306,
   uri: env.URI
 };
 
